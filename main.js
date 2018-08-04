e8869d372dc91527be119982e01164f01319e9e0
Bensu <bnsue1@gmail.com>
2018-08-03 22:17:15 -0700

    Removed content fading

--- main.js
+++ main.js
@@ -43,22 +43,22 @@ $(document).ready(function(){
   });
 });
 
-// Fade divs
-$(window).on("load",function() {
-  $(window).scroll(function() {
-    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-    $(".fade").each(function() {
-      /* Check the location of each desired element */
-      var objectBottom = $(this).offset().top + $(this).outerHeight();
-      var objectTop = $(this).offset().top + 20;
+// // Fade divs
+// $(window).on("load",function() {
+//   $(window).scroll(function() {
+//     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+//     $(".fade").each(function() {
+//       /* Check the location of each desired element */
+//       var objectBottom = $(this).offset().top + $(this).outerHeight();
+//       var objectTop = $(this).offset().top + 20;
       
-      /* If the element is completely within bounds of the window, fade it in */
-      if (objectTop < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-      }
-    });
-  }).scroll(); //invoke scroll-handler on page-load
-});
+//       /* If the element is completely within bounds of the window, fade it in */
+//       if (objectTop < windowBottom) { //object comes into view (scrolling down)
+//         if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+//       }
+//     });
+//   }).scroll(); //invoke scroll-handler on page-load
+// });
 
 // Display navbar on small screen
 function toggleNavbar() {
